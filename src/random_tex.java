dc0d3ed5f7c95447c446d18e8b22db4d854e26b2
Wavyspla9980 <tr0lich@yandex.ru>
2024-09-18 12:39:50 +0300

    Delete src directory

--- src/random_tex.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.util.Random;
-
-public class random_tex extends JFrame {
-    Random random = new Random();
-    int a = random.nextInt(500);
-    int b = random.nextInt(599);
-    JLabel label = new JLabel("number:"+a+b);
-}
